d66365e601b10aaa97d3ab21c06b397f255f1c87
Ogur4ick <75222531+Ogur4ick@users.noreply.github.com>
2023-04-13 23:52:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import numpy as np
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import pandas as pd 
+import numpy as np 
+from scipy.stats import ttest_rel 
+ 
+ 
+chat_id = 968976840 # Ваш chat ID, не меняйте название переменной 
+ 
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
+    t_statistic, p_value = ttest_rel(x, y) 
+    alpha = 0.02 
+    return p_value < alpha
